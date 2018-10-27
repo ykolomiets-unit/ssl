872fe497b9d619b724fd8637a33f3069fd80ebbd
Kolomiets Yaroslav <kolomiets.y.n@gmail.com>
2018-10-27 18:28:14 +0300

    HMAC norminneted

--- includes/hmac.h
+++ includes/hmac.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hmac.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykolomie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/27 18:19:39 by ykolomie          #+#    #+#             */
+/*   Updated: 2018/10/27 18:20:41 by ykolomie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef HMAC_H
 # define HMAC_H
 
@@ -5,35 +17,36 @@
 # include "md5.h"
 # include "sha224_256.h"
 
-typedef void	(*hash_init)(void *ctx);
-typedef void	(*hash_update)(void *ctx, t_byte *input, uint32_t input_len);
-typedef void	(*hash_final)(void *ctx, t_byte *digest);
+typedef void		(*t_hash_init)(void *ctx);
+typedef void		(*t_hash_update)(void *ctx,
+						t_byte *input, uint32_t input_len);
+typedef void		(*t_hash_final)(void *ctx, t_byte *digest);
 
-typedef struct	s_hmac_help
+typedef struct		s_hmac_help
 {
-	void		*ctx;
-	t_byte		*ipad;
-	t_byte		*opad;
-	uint32_t	block_size;
-	uint32_t	digest_size;
-	hash_init	init;
-	hash_update	update;
-	hash_final	final;
-	t_byte		*newkey;
-}				t_hmac_help;
+	void			*ctx;
+	t_byte			*ipad;
+	t_byte			*opad;
+	uint32_t		block_size;
+	uint32_t		digest_size;
+	t_hash_init		init;
+	t_hash_update	update;
+	t_hash_final	final;
+	t_byte			*newkey;
+}					t_hmac_help;
 
-typedef struct	s_hmac_params
+typedef struct		s_hmac_params
 {
-	t_byte		*text;
-	uint32_t	text_len;
-	t_byte		*key;
-	uint32_t	key_len;
-}				t_hmac_params;
+	t_byte			*text;
+	uint32_t		text_len;
+	t_byte			*key;
+	uint32_t		key_len;
+}					t_hmac_params;
 
-void			hmac_md5(t_hmac_params params,
-					t_byte digest[MD5_DIGEST_SIZE]);
+void				hmac_md5(t_hmac_params params,
+						t_byte digest[MD5_DIGEST_SIZE]);
 
-void			hmac_sha256(t_hmac_params params,
-					t_byte digest[SHA256_DIGEST_SIZE]);
+void				hmac_sha256(t_hmac_params params,
+						t_byte digest[SHA256_DIGEST_SIZE]);
 
 #endif
